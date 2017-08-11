66c8e6dfffbac24aa0146e1f3d2a9cf703e6acaf
Maxim Baz <git@maximbaz.com>
2017-08-11 17:34:11 +0200

    Show all renaming errors, don't abort after the first issue

--- rename_files.py
+++ rename_files.py
@@ -23,26 +23,35 @@ if not os.path.isdir(images_path):
 metadata = json.load(open(emoji_json_path))
 images = os.listdir(images_path)
 
+success = True
+
 for img in images:
     img_name = img[:-4]
     img_ext = img[-4:]
     if img_name not in metadata:
-        print("No metadata for image file '{}' in '{}', aborting.".format(
+        print("No metadata for image file '{}' in '{}'".format(
             img_name, emoji_json_path))
-        sys.exit(1)
+        success = False
+        continue
     if 'code_points' not in metadata[img_name]:
-        print("No code_points metadata for image file '{}' in '{}', aborting.".
-              format(img_name, emoji_json_path))
-        sys.exit(1)
+        print("No code_points metadata for image file '{}' in '{}'".format(
+            img_name, emoji_json_path))
+        success = False
+        continue
     if 'non_fully_qualified' not in metadata[img_name]['code_points']:
         print(
-            "No 'non_fully_qualified' code_point for image file '{}' in '{}', aborting.".
+            "No 'non_fully_qualified' code_point for image file '{}' in '{}'".
             format(img_name, emoji_json_path))
-        sys.exit(1)
+        success = False
+        continue
     old_img_path = os.path.join(images_path, img)
     new_img_path = os.path.join(
         images_path,
         metadata[img_name]['code_points']['non_fully_qualified'] + img_ext)
     os.rename(old_img_path, new_img_path)
 
-print("Images were renamed successfully.")
+if success:
+    print("Images were renamed successfully.")
+else:
+    print("There was an error while renaming images, aborting.")
+    sys.exit(1)
