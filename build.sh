2fa71688274de47105e7f4330f7301dea23e4ca2
Maxim Baz <git@maximbaz.com>
2020-01-30 20:45:04 +0100

    Build alternative font if assets are available

--- build.sh
+++ build.sh
@@ -18,3 +18,20 @@ NOCOMPRESSING=1 make
 
 echo "==> Saving the font file on the host..."
 mv JoyPixels.ttf /fonts/joypixels-android.ttf
+
+[ ! -d /assets/png/128/_alt/ ] && exit 0
+
+echo "==> Copying alternative assets for further processing..."
+rm -rf png
+mkdir -p png/128/
+cp /assets/png/128/*.png png/128/
+cp /assets/png/128/_alt/*.png png/128/
+
+echo "==> Renaming image files..."
+/rename_files.py /assets/emoji.json /root/joypixels-android/png/128
+
+echo "==> Building the alternative font..."
+NOCOMPRESSING=1 make
+
+echo "==> Saving the alternative font file on the host..."
+mv JoyPixels.ttf /fonts/joypixels-android-alt.ttf
